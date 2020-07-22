d5d2dc9e5bb49b4364b8d9791670ab9c528f0841
KevinNThomas <me@kevinthomas.dev>
2020-07-21 22:00:44 -0700

    Change layout, add color theme support
    - Modify CSS to be more mobile friendly
    - Modify CSS to support color themes. These themes are configured using
      environment variables that can be passed into the docker container
    - Add support for custom color themes, these are also set through
      environment variables

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io"
 	"log"
+	"os"
 
 	"github.com/namsral/flag"
 	"github.com/prologic/bitcask"
@@ -28,5 +30,56 @@ func main() {
 	}
 	defer db.Close()
 
+	selectColorTheme()
+
 	newServer(bind).listenAndServe()
+}
+
+func selectColorTheme() {
+	colorTheme := os.Getenv("COLOR_THEME")
+
+	if colorTheme == "custom" {
+		pageBackgroundColor := os.Getenv("COLOR_PAGE_BACKGROUND")
+		inputBackgroundColor := os.Getenv("COLOR_INPUT_BACKGROUND")
+		foregroundColor := os.Getenv("COLOR_FOREGROUND")
+		checkMarkColor := os.Getenv("COLOR_CHECK_MARK")
+		xMarkColor := os.Getenv("COLOR_X_MARK")
+		labelColor := os.Getenv("COLOR_LABEL")
+
+		customThemeFile, err := os.OpenFile("./static/css/color-theme.css", os.O_RDWR|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer customThemeFile.Close()
+
+		_, err = customThemeFile.WriteString(":root {" +
+			"\n\t--page-background: #" + pageBackgroundColor + ";" +
+			"\n\t--input-background: #" + inputBackgroundColor + ";" +
+			"\n\t--foreground: #" + foregroundColor + ";" +
+			"\n\t--check: #" + checkMarkColor + ";" +
+			"\n\t--x: #" + xMarkColor + ";" +
+			"\n\t--label: #" + labelColor + ";" +
+			"\n}")
+	} else {
+		if colorTheme == "" {
+			colorTheme = "dracula"
+		}
+
+		from, err := os.Open("./static/color-themes/" + colorTheme + ".css")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer from.Close()
+
+		to, err := os.OpenFile("./static/css/color-theme.css", os.O_RDWR|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer to.Close()
+
+		_, err = io.Copy(to, from)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
