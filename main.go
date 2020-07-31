28cac90ba619a6102468406c375531bcabf18151
KevinNThomas <me@kevinthomas.dev>
2020-07-30 18:26:23 -0700

    Fix git conflicts, use flag package for parsing environment variables

--- main.go
+++ main.go
@@ -13,12 +13,16 @@ var (
 
 func main() {
 	var (
-		dbpath string
-		bind   string
+		dbpath           string
+		bind             string
+		MAX_ITEMS        int
+		MAX_TITLE_LENGTH int
 	)
 
 	flag.StringVar(&dbpath, "dbpath", "todo.db", "Database path")
 	flag.StringVar(&bind, "bind", "0.0.0.0:8000", "[int]:<port> to bind to")
+	flag.IntVar(&MAX_ITEMS, "MAX_ITEMS", 100, "maximum number of items allowed in the todo list")
+	flag.IntVar(&MAX_TITLE_LENGTH, "MAX_TITLE_LENGTH", 100, "maximum valid length of a todo item's title")
 	flag.Parse()
 
 	var err error
@@ -28,5 +32,5 @@ func main() {
 	}
 	defer db.Close()
 
-	newServer(bind).listenAndServe()
+	newServer(bind, MAX_ITEMS, MAX_TITLE_LENGTH).listenAndServe()
 }
