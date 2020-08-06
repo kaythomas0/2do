056d6c1a6554cc5259ffcf4709c704f585cb589e
KevinNThomas <me@kevinthomas.dev>
2020-08-05 18:10:42 -0700

    Remove hyphens from flag env variable names

--- main.go
+++ main.go
@@ -13,16 +13,16 @@ var (
 
 func main() {
 	var (
-		dbpath           string
-		bind             string
-		maxItems        int
+		dbpath         string
+		bind           string
+		maxItems       int
 		maxTitleLength int
 	)
 
 	flag.StringVar(&dbpath, "dbpath", "todo.db", "Database path")
 	flag.StringVar(&bind, "bind", "0.0.0.0:8000", "[int]:<port> to bind to")
-	flag.IntVar(&maxItems, "max-items", 100, "maximum number of items allowed in the todo list")
-	flag.IntVar(&maxTitleLength, "max-title-length", 100, "maximum valid length of a todo item's title")
+	flag.IntVar(&maxItems, "maxitems", 100, "maximum number of items allowed in the todo list")
+	flag.IntVar(&maxTitleLength, "maxtitlelength", 100, "maximum valid length of a todo item's title")
 	flag.Parse()
 
 	var err error
